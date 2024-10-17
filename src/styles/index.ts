9d90fa76b332d42227d25465c1e6d31bface5d22
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-17 02:46:05 -0300

    feat: add and configure stitches

---
+++ src/styles/index.ts
@@ -0,0 +1,17 @@
+import { createStitches } from "@stitches/react"
+
+export const {
+  config,
+  styled,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+} = createStitches({
+  theme: {
+    colors: {
+      purple: "#8257e6",
+    },
+  },
+})
