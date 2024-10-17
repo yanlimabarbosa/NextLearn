954e5d7c126ab33a7be1e0fb878d3254f2a95f18
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-17 07:44:02 -0300

    feat: Stripe integration

---
+++ src/lib/stripe.ts
@@ -0,0 +1,8 @@
+import Stripe from "stripe"
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2024-09-30.acacia",
+  appInfo: {
+    name: "Ignite Shop",
+  },
+})
