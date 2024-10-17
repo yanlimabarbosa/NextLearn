98e8ccd25a48e4edc97c0e0bf847b65e5bdd3f7f
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-17 02:56:02 -0300

    feat: add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
-function MyApp({ Component, pageProps }) {
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+
+globalStyles()
+
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
 
