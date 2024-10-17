a425431b29572f0309ae5086ebb8a1f9c581060d
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-17 04:24:21 -0300

    feat: add header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,17 @@ import { globalStyles } from "../styles/global"
 
 globalStyles()
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import logoImg from "../assets/logo.svg"
+import { Container, Header } from "../styles/pages/app"
 
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
+}
