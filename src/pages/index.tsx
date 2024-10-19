a8d777dac320fa018cbe7e7abd96fc10141367f8
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-19 18:53:44 -0300

    feat: Implement static site generation for the home page
    This commit switches the home page from server-side rendering to static site generation using `getStaticProps`. This improves performance and SEO by pre-rendering the page content at build time. Additionally, `revalidate` is set to 2 hours to ensure that the page content is refreshed periodically.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,10 @@
 import Image from "next/image"
-import { styled } from "../styles"
 import { HomeContainer, Product } from "../styles/pages/home"
-
-import camiseta1 from "../assets/camisetas/1.png"
-import camiseta2 from "../assets/camisetas/2.png"
-import camiseta3 from "../assets/camisetas/3.png"
 
 import "keen-slider/keen-slider.min.css"
 import { useKeenSlider } from "keen-slider/react"
 import { stripe } from "../lib/stripe"
-import { GetServerSideProps } from "next"
+import { GetStaticProps } from "next"
 import Stripe from "stripe"
 
 interface HomeProps {
@@ -47,7 +42,7 @@ export default function Home({ products }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ["data.default_price"],
   })
@@ -68,5 +63,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 2, // 2 hours
   }
 }
