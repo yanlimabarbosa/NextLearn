1a1e644ad55fb47eea3197c416ce8eaa32a10167
Yan Lima Barbosa <yanbr763@gmail.com>
2024-10-22 07:38:09 -0300

    feat: success page structure

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,20 @@
+import Link from "next/link"
+import { ImageContainer, SuccessContainer } from "../styles/pages/success"
+
 export default function Success() {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra Efetuada!</h1>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhull <strong>nome</strong>, sua <strong>nome</strong> já está no a
+        caminho da sua casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
   )
 }
